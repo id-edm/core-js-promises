82f61e9387dd4870e6707606f6b2f827e0652e72
id-edm <ivandubochit@gmail.com>
2025-04-15 01:15:19 +0400

    feat: add solution queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -136,8 +136,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(async (accumulatorPromise, currentPromise) => {
+    const accumulatedString = await accumulatorPromise;
+    const value = await currentPromise;
+    return accumulatedString + value;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
